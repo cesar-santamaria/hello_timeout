a6e4d00de2eb792b7de1855bd3a102c4db336089
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-12 17:14:00 -0500

    complete hello_timeout task

--- hello_timeout.js
+++ hello_timeout.js
@@ -1,3 +1,6 @@
+/* 
+Struggled with this excercise. Missed adding delay to setTimeout which caused the code to return all three values at once instead of 1 second apart for each.
+*/
 let inputValues = ['4311o','th3r3','w0r1d'];
 let delay = 0;
 
